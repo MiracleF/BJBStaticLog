58f19e3894995fdb4df8f92a9ded8690aa6e5fb2
JayLee <13660180195@163.com>
2021-04-09 15:29:30 +0800

    Initial Commit

---
+++ BJBStaticLog/BJBStaticLog.h
@@ -0,0 +1,12 @@
+//
+//  BJBStaticLog.h
+//  BJBStaticLog
+//
+//  Created by 李杰峰 on 2021/4/9.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BJBStaticLog : NSObject
+
+@end
---
+++ BJBStaticLog/BJBStaticLog.m
@@ -0,0 +1,12 @@
+//
+//  BJBStaticLog.m
+//  BJBStaticLog
+//
+//  Created by 李杰峰 on 2021/4/9.
+//
+
+#import "BJBStaticLog.h"
+
+@implementation BJBStaticLog
+
+@end
